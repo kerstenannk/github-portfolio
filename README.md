e53d5426bfe6e47a2e908e5df167b5cce48bf48c
Kersten Kaufman <kaufmancoordinations@gmail.com>
2018-07-16 19:48:55 -0400

    Added text

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a few of the homework assignments we have had with their answers.
---
+++ answers.js
@@ -0,0 +1,22 @@
+"Sam I Am"
+"Now that we’ve gotten a little taste of ruby, here’s a challenge for you: Remember to always submit code samples using markdown. You’re using Ruby so use ruby markdown. Submit a code sample that outputs “Sam” 3 times, each on a new line. This is what we’re going for:"
+`Sam`
+`Sam`
+`Sam`
+
+"Answer"
+```ruby
+3.times do
+puts "Sam"
+end
+```
+"Intro to HTML"
+"Objective: Identify 5 html tags and submit them using inline markdown. Submit a list of 5 tags you can use with HTML, and a short explanation of what each is used for (remember to use Markdown!). Example submission(do not use this as one of your five):"
+"<video></video> - Used for video playback"
+
+"Answer"
+"<p></p> Used for paragraphs"
+"<audio></audio> Used for adding audio"
+"<img></img> Used for adding images"
+"<article></article> Used for adding article links"
+"<li></li> Used for making lists"
